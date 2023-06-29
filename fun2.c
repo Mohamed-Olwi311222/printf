1307806205aeb548ebc9ab91f7fff9c9ef503e08
AceMido <acemido2004@gmail.com>
2023-06-29 07:23:38 +0300

    test 4

--- fun2.c
+++ fun2.c
@@ -1,5 +1,36 @@
 #include "main.h"
-#include <math.h>
+/**
+ * my_log - returns the log of @x
+ * @x: double type
+ * Return: return the log(x)
+ */
+double my_log(double x)
+{
+	double y = 1.0;
+	double sum = 0.0;
+	int i;
+
+	if (x <= 0.0)
+	{
+		return NAN;
+	}
+
+	for (i = 0; i < 100; i++)
+	{
+		sum += y / (2.0 * i + 1.0);
+		y *= (x - 1.0) / (x + 1.0) * (x - 1.0) / (x + 1.0);
+	}
+	return (2.0 * sum);
+}
+/**
+ * my_log10 - return the log10 of @x
+ * @x: double type
+ * Return: log10(x)
+ */
+double my_log10(double x)
+{
+	return (my_log(x) / my_log(10));
+}
 /**
  * print_int - prints an int
  * @vars: variable to hold an int
@@ -9,7 +40,6 @@ int print_int(va_list vars)
 {
 	char *str;
 	int num = va_arg(vars, int), count = 0, i;
-	int size = (int) log10(abs(num));
 
 	if (num < 0)
 		count += _putchar('-');
@@ -18,7 +48,7 @@ int print_int(va_list vars)
 	 * in 2nd line 12345 => 1234
 	 * and repeates
 	 */
-	str = malloc(1 + size);
+	str = malloc(1 + my_log10(abs(num)));
 	if (str == NULL)
 		return (0);
 
@@ -39,7 +69,7 @@ int print_int(va_list vars)
  */
 int print_double(va_list vars)
 {
-	double num = va_arg(vars, double), decimal, size;
+	double num = va_arg(vars, double), decimal;
 	int count = 0, i, digit, int_part;
 	char *str_int;
 
@@ -50,8 +80,7 @@ int print_double(va_list vars)
 	}
 
 	int_part = (int) num;
-	size = log10(abs(int_part));
-	str_int = malloc(1 + size);
+	str_int = malloc(1 + my_log10(abs(int_part)));
 	if (str_int == NULL)
 		return (0);
 
